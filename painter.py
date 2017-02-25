d9ea77f145c71caa31b744c4219ba44e82798637
Jerry Li <jerrylijiaming@gmail.com>
2017-02-26 09:05:05 +1100

    Added code for merging n style checkpoints. It works but the problem is that the combined version is not as good as the not combined one.

--- painter.py
+++ painter.py
@@ -4,14 +4,14 @@ import n_style_out_net
 
 
 class Painter(object):
-    def __init__(self, save_dir,num_styles, gpu=0, gpu_fraction = 0.5, mat_dir = 'imagenet-vgg-verydeep-19.mat', do_save_npy = False, npy_path = None,):
+    def __init__(self, save_dir,num_styles, gpu=0, gpu_fraction = 0.5, mat_dir = 'imagenet-vgg-verydeep-19.mat', do_load_from_npy = False, npy_path = None,):
 
         print u"start"
         self.root = u"./static/images/"
         self.batchsize = 1
         self.outdir = self.root + u"out/"
         self.painter = n_style_out_net.Stylizer(mat_dir,128,128,num_styles, use_johnson=True, save_dir=save_dir,
-                                                do_save_npy=do_save_npy,npy_path=npy_path,
+                                                do_load_from_npy=do_load_from_npy, npy_path=npy_path,
                                                 gpu_id=gpu, gpu_fraction=gpu_fraction)
         print u"load model"
 
